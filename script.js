61a86d115361ed60770afc6b8a3efcf8e88147b4
mane <abdulwahablawal@gmail.com>
2025-01-16 12:18:29 +0100

    wahab

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function cartApp() {
     return {
         items: [
-            { id: 1, name: 'Waffle with Berries', price: 6.50, image: '../src/image/image-waffle-desktop.jpg', inCart: false, quantity: 0 },
-            { id: 2, name: 'Vanilla Bean Crème Brûlée', price: 7.00, image: '../src/image/image-creme-brulee-desktop.jpg', inCart: false, quantity: 0 },
-            { id: 3, name: 'Macaron Mix of Five', price: 8.00, image: '../src/image/image-macaron-desktop.jpg', inCart: false, quantity: 0 },
-            { id: 4, name: 'Classic Tiramisu', price: 5.50, image: '../src/image/image-tiramisu-desktop.jpg', inCart: false, quantity: 0 },
-            { id: 5, name: 'Pistachio Baklava', price: 4.00, image: '../src/image/image-baklava-desktop.jpg', inCart: false, quantity: 0 },
-            { id: 6, name: 'Lemon Meringue Pie', price: 5.00, image: '../src/image/image-meringue-desktop.jpg', inCart: false, quantity: 0 },
-            { id: 7, name: 'Red Velvet Cake', price: 4.50, image: '../src/image/image-cake-desktop.jpg', inCart: false, quantity: 0 },
-            { id: 8, name: 'Salted Caramel Brownie', price: 5.50, image: '../src/image/image-brownie-desktop.jpg', inCart: false, quantity: 0 },
-            { id: 9, name: 'Vanilla Panna Cotta', price: 6.50, image: '../src/image/image-panna-cotta-desktop.jpg', inCart: false, quantity: 0 }
+            { id: 1, name: 'Waffle with Berries', price: 6.50, price: "6.50", image: '../src/image/image-waffle-desktop.jpg', inCart: false, quantity: 0, specify: 'waffle' },
+            { id: 2, name: 'Vanilla Bean Crème Brûlée', price: 7.00, price: "7.00", image: '../src/image/image-creme-brulee-desktop.jpg', inCart: false, quantity: 0, specify: 'creme brulee' },
+            { id: 3, name: 'Macaron Mix of Five', price: 8.00, price: "8.00", image: '../src/image/image-macaron-desktop.jpg', inCart: false, quantity: 0, specify: 'macaron' },
+            { id: 4, name: 'Classic Tiramisu', price: 5.50, price: "5.50", image: '../src/image/image-tiramisu-desktop.jpg', inCart: false, quantity: 0, specify: 'tiramisu' },
+            { id: 5, name: 'Pistachio Baklava', price: 4.00, price: "4.00", image: '../src/image/image-baklava-desktop.jpg', inCart: false, quantity: 0, specify: 'baklava' },
+            { id: 6, name: 'Lemon Meringue Pie', price: 5.00, price: "5.00", image: '../src/image/image-meringue-desktop.jpg', inCart: false, quantity: 0, specify: 'pie' },
+            { id: 7, name: 'Red Velvet Cake', price: 4.50, price: "4.50", image: '../src/image/image-cake-desktop.jpg', inCart: false, quantity: 0, specify: 'cake' },
+            { id: 8, name: 'Salted Caramel Brownie', price: 5.50, price: "5.50", image: '../src/image/image-brownie-desktop.jpg', inCart: false, quantity: 0, specify: 'brownie' },
+            { id: 9, name: 'Vanilla Panna Cotta', price: 6.50, price: "6.50", image: '../src/image/image-panna-cotta-desktop.jpg', inCart: false, quantity: 0, specify: 'panna otta' }
         ],
         cart: [],
         orderConfirmed: false,
